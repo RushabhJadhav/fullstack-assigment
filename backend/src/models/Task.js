eb26f0cf2ff1656fe66a54dce00ddd82fbdf297b
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-05-05 20:24:39 +0530

    env and task model setup

---
+++ backend/src/models/Task.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    requried: true
+  },
+  desscription: {
+    type: String,
+  },
+  status: {
+    type: Boolean,
+  },
+}, {
+  timestamps: true
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Note;
